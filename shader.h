836583ea089ab2665e9e590801f19749ae801698
AkkarinJung <ths40834@ths.hal.ac.jp>
2025-12-23 00:58:25 +0900

    Animation

--- shader.h
+++ shader.h
@@ -13,6 +13,9 @@
 #include <DirectXMath.h>
 #include "direct3d.h"
 
+#include "model.h"
+#define MAX_BONES 100
+
 bool Shader_Initialize(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
 void Shader_Finalize();
 
@@ -25,6 +28,8 @@ void Shader_SetShadowMatrix(const DirectX::XMMATRIX& mat); // Set at b3
 void Shader_SetShadowMap(ID3D11ShaderResourceView* shadowSRV); // t1
 void Shader_SetShadowSampler(ID3D11SamplerState* sampler); // s1
 void Shader_SetShadowLightData(const DirectX::XMFLOAT3& pos, float radius, float intensity, float shadowPassMode = 0.0f);
+
+void Shader_SetBones(MODEL* model);
 
 void Shader_Begin();
 
