a1b39c237e255a40d1547827571b9de79ea0f970
IrokawaRikuto <ths40195@ths.ac.jp>
2026-02-03 10:00:14 +0900

    newKeyBind追加遂にリビルドしなくても大丈夫！！！ それに伴ってinclude修正

--- newKeyBind.cpp
+++ newKeyBind.cpp
@@ -1,19 +1,19 @@
 //KeyBind.cpp
-#include "KeyBind.h"
+#include "newKeyBind.h"
 #include "Controller.h" // 必要な定義をインクルード
 
-InputKey UpKey =		{ KK_W,			XINPUT_GAMEPAD_DPAD_UP };	//前進
-InputKey RightKey =		{ KK_D,			XINPUT_GAMEPAD_DPAD_RIGHT };//右移動
-InputKey DownKey =		{ KK_S,			XINPUT_GAMEPAD_DPAD_DOWN };	//後退
-InputKey LeftKey =		{ KK_A,			XINPUT_GAMEPAD_DPAD_LEFT };	//左移動
+InputKey UpKey = { KK_W,			XINPUT_GAMEPAD_DPAD_UP };	//前進
+InputKey RightKey = { KK_D,			XINPUT_GAMEPAD_DPAD_RIGHT };//右移動
+InputKey DownKey = { KK_S,			XINPUT_GAMEPAD_DPAD_DOWN };	//後退
+InputKey LeftKey = { KK_A,			XINPUT_GAMEPAD_DPAD_LEFT };	//左移動
 
-InputKey JumpKey =		{ KK_SPACE,		XINPUT_GAMEPAD_A };			//ジャンプ
-InputKey DashKey =		{ KK_LEFTSHIFT,	XINPUT_GAMEPAD_X };			//ダッシュ
-InputKey ActionKey =	{ KK_F,			XINPUT_GAMEPAD_B };			//アクション
-InputKey ChangeKey =	{ KK_F,			XINPUT_GAMEPAD_B };			//影変身
+InputKey JumpKey = { KK_SPACE,		XINPUT_GAMEPAD_A };			//ジャンプ
+InputKey DashKey = { KK_LEFTSHIFT,	XINPUT_GAMEPAD_X };			//ダッシュ
+InputKey ActionKey = { KK_F,			XINPUT_GAMEPAD_B };			//アクション
+InputKey ChangeKey = { KK_F,			XINPUT_GAMEPAD_B };			//影変身
 
-InputKey ResetKey =		{ KK_R,			XINPUT_GAMEPAD_BACK };		//リセット
-InputKey MenuKey =		{ KK_ESCAPE,	XINPUT_GAMEPAD_START };		//メニュー
+InputKey ResetKey = { KK_R,			XINPUT_GAMEPAD_BACK };		//リセット
+InputKey MenuKey = { KK_ESCAPE,	XINPUT_GAMEPAD_START };		//メニュー
 
 //押している時
 bool IsInputPress(const InputKey& key, const Controller& pad)
