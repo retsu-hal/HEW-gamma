86eaf0058afcf6326be95c85a5eb2f67ec082474
ChenBolUo <boluo819ber@gmail.com>
2026-02-23 20:08:42 +0900

    Seesaw2.0

--- FieldSeesaw.h
+++ FieldSeesaw.h
@@ -24,7 +24,7 @@ struct SeesawParams
 
     SeesawParams()
         : tiltAngle(0.0f)
-        , maxTiltAngle(15.0f)
+        , maxTiltAngle(12.0f)
         , tiltSpeed(60.0f)
         , returnSpeed(0.0f)
         , boardLength(2.5f)
